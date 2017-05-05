02bbdc34ec14265badc75282ba42710ad6183e8b
Kurtis Houser <houserkurtis@gmail.com>
2017-05-04 17:33:20 -0700

    Complete basic functionality

---
+++ js/index.js
@@ -0,0 +1,58 @@
+$(function() {
+
+  console.log("JQ Loaded. Let's do some basic math!");
+
+  let output = '';
+  let evaluated = false;
+
+  $('.buttons').children().on('click', function(event) {
+
+    var $target = $(event.target);
+
+    console.log('clicked', $target, $target.html(), typeof $target.html(), $target.text());
+
+    if ($target.hasClass('operator')) { // operator buttons
+
+      console.log('operator');
+
+      if ($target.is('#clear')) { // C operator
+
+        output = '';
+        $('#screen').html(output);
+
+      } else if ($target.is('#equals')) { // = operator, evaluate the formula
+
+        output = output.replace(/x/g, '*');
+        output = output.replace(/÷/g, '/');
+        output = eval(output);
+        $('#screen').html(output);
+        evaluated = true;
+
+      } else { // other operators
+
+        output += $target.text();
+        $('#screen').html(output);
+
+      }
+
+    } else if ($target.is('#zero')) { // zero button
+
+      // zero will eventually be handled differently
+      // depending on the contents in the display
+      console.log('zero');
+      output += $target.text();
+      $('#screen').html(output);
+
+    } else { // other number buttons
+
+      console.log('number');
+
+      output += $target.text();
+      $('#screen').html(output);
+
+    }
+
+  }); // end click handler
+
+
+});
